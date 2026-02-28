f8eadb77ab69a17b8d2ef282688af045a208ed9f
pascalecloset1994 <pascale.closet1994@gmail.com>
2026-02-27 21:55:41 -0300

    updated index for vercel

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-import { createApp } from "./app.js";
-import { pgRailwayDB } from "./config/dbConfig.js";
+import express from "express";
+import cookieParser from "cookie-parser";
+import { createAppRouter } from "./routes/index.js";
+import { corsMiddleware } from "./middlewares/cors.js";
+import rateLimit from "express-rate-limit";
 
-const app = createApp({ db: pgRailwayDB });
+ const app = express();
 
-export default app;
+  const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000,
+    max: 300,
+    message: "Demasiadas peticiones desde esa IP. Reintente más tarde..",
+  });
+
+  app.use(limiter);
+  app.use(corsMiddleware());
+  app.use(cookieParser());
+  app.use(express.json({ limit: "10mb" }));
+  app.use(express.urlencoded({ extended: false, limit: "10mb" }));
+  app.disable("x-powered-by");
+  app.use("/api", createAppRouter({ db }));
+
+  export default app;
