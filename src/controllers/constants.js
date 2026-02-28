16091c2987091cfad521c5a64c0a4b0f3ddd146a
pascalecloset1994 <pascale.closet1994@gmail.com>
2026-02-28 02:08:08 -0300

    query fix

--- src/controllers/constants.js
+++ src/controllers/constants.js
@@ -11,7 +11,7 @@ const DELETE_USER = "DELETE FROM pascale_users WHERE user_id = $1 RETURNING *;";
 
 // USER EDITABLE CONTENT
 const GET_HERO = "SELECT hero_collection, hero_title, hero_subtitle, hero_url_image, hero_updated, hero_updated_at FROM user_content WHERE id = 1;";
-const GET_FOOTER = "SELECT footer_title, footer_location, footer_schedule, footer_ur_image, footer_updated, footer_updated_at FROM user_content WHERE id = 1;";
+const GET_FOOTER = "SELECT footer_title, footer_location, footer_schedule, footer_url_image, footer_updated, footer_updated_at FROM user_content WHERE id = 1;";
 const UPDATE_HERO = "UPDATE user_content SET updated_at = NOW(), hero_collection = $2, hero_title = $3, hero_subtitle = $4, updated = $5, url_image = $6 WHERE ID = $1 RETURNING *;"
 const UPDATE_FOOTER = "UPDATE user_content SET updated_at = NOW(), title = $2, location = $3, schedule = $4, updated = $5, url_image = $6 WHERE ID = $1 RETURNING *;"
 
