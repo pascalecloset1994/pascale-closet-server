01c1de40668e01d01899626cda33fbcbe0d118fb
pascalecloset1994 <pascale.closet1994@gmail.com>
2026-02-28 04:30:33 -0300

    updated web socket config

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,9 +1,12 @@
-import { Pool } from "@neondatabase/serverless";
+import { Pool, neonConfig } from "@neondatabase/serverless";
 import path from "path";
+import ws from "ws"
 
 if (process.env.NODE_ENV !== "production") {
   process.loadEnvFile(path.join(process.cwd(), "/.env"));
 }
+
+neonConfig.webSocketConstructor = ws;
 
 export const neonDB = new Pool({
   connectionString: process.env.DATABASE_URL
