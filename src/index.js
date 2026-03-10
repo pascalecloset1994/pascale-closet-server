d7db9b92bdcd4bb966e1bd9496d962b4657b65eb
pascalecloset1994 <pascale.closet1994@gmail.com>
2026-03-10 00:03:14 -0300

    updated limit size

--- src/index.js
+++ src/index.js
@@ -10,15 +10,15 @@ app.set("trust proxy", 1);
 
 const limiter = rateLimit({
     windowMs: 10 * 60 * 1000,
-    max: 300,
+    max: 500,
     message: "Demasiadas peticiones desde esa IP. Reintente más tarde..",
 });
 
 app.use(limiter);
 app.use(corsMiddleware());
 app.use(cookieParser());
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: false, limit: "10mb" }));
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ extended: false, limit: "25mb" }));
 app.disable("x-powered-by");
 app.use(createAppRouter({ db: neonDB }));
 
