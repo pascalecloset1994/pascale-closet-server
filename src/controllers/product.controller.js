6c2d80c25e74c8ecfa96cc2c5fbf07cd8f007aa1
pascalecloset1994 <pascale.closet1994@gmail.com>
2026-03-30 04:44:10 -0300

    *-

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -11,10 +11,10 @@ export class ProductController {
       if (products.length === 0)
         return res.json({ message: "No hay productos en la base de datos." });
 
-      const reviews = await this.model.getAllProductReviews();
+      const reviews = await this.model.getAllProductsReviews();
 
       if (Array.isArray(products) && reviews.length > 0) {
-        products.push(reviews);
+        products.push(...reviews)
       }
 
       return res.status(200).json(products);
